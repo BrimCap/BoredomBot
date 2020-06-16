5684b67971e5a042e4e6bbecd87a07e60eb2a4fb
BrimCap <vandithvponad123@gmail.com>
2020-06-16 11:04:23 +0530

    Added kicking

--- cogs/poof.py
+++ cogs/poof.py
@@ -7,12 +7,14 @@ class poofer(commands.Cog):
         self.client = client
 
     @commands.command()
-    async def poof(self, ctx, member : discord.Member):
+    async def poof(self, ctx, member : discord.Member, reason = None):
 
         if member.id == self.client.user.id:
             await ctx.send('Did you just poof me? smh')
         else:
             await ctx.send(f'You have been poofed {member.mention}')
+            await member.kick(reason = reason)
+            await member.send('You have been poofed sadly :(')
 
     @poof.error
     async def poof_error_handler(self, ctx, error):
