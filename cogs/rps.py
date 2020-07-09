8ee659a963193500813a72162b7b1d899be642fe
BrimCap <vandithvponad123@gmail.com>
2020-07-09 21:28:39 +0530

    Improved rps command

--- cogs/rps.py
+++ cogs/rps.py
@@ -1,143 +1,101 @@
 import discord
-import random
 from discord.ext import commands
-from asyncio import TimeoutError
 
-class rps(commands.Cog):
+class Rps(commands.Cog):
 
     def __init__(self, client):
-        self.client = client 
+        self.client = client
+
+   
 
     @commands.command()
-    async def rps(self, ctx, member : discord.Member = None):
+    async def rps(self, ctx, target : discord.Member = None):
 
-        if member:
+        
+        
+        start = discord.Embed(
+            color = 0x00FFEC,
+            title = f"{ctx.author.name} vs {target.name}",
+            description = f"Both of you check your dms to start playing!"
+        )
 
-            await ctx.send(f'{member.mention}! {ctx.author.mention} has challenged you to an **RPS** duel! Reply with `yes` or `no`')
+        # <!-- Functions -->
 
-            def confirm(message : discord.Message):
-                return message.author.id == member.id and message.content.lower() and message.channel.id == ctx.channel.id
+        async def pick(member : discord.Member):
 
-            try:
-                confirmation = await self.client.wait_for('message', check = confirm, timeout = 20)
-            except TimeoutError:
-                await ctx.send(f'{member.mention} took too long so the match has been cancelled')
+            choices = [
+                "rock",
+                "paper",
+                "scissors"
+            ]
 
-            if confirmation.content.lower() == 'yes':
-                pass
-            else:
-                await ctx.send(f'{member.mention} has declined the challenge')
-                return
+            def wait_for_choice(message: discord.Message):
+                return message.author == member and not message.guild and message.content.lower() in choices
 
+            await member.send("Choose `rock`, `paper` or `scissors`")
 
-            starting = discord.Embed(
-                colour = 0xadd8e6,
-                title = f'The Game Is Starting!',
-                description = f'{member.mention} and {ctx.author.mention} check your dms!' 
-            )
+            choice = await self.client.wait_for('message', check = wait_for_choice)
 
-            await ctx.send(embed = starting)
+            await member.send("Decision Made.")
+            return choice.content.lower() 
 
-            await ctx.author.send('Opponent making a decision...')
-            await member.send('Choose `rock`, `paper` or `scissors`')
+        def win(targeted, author):
+            if targeted == 'rock':
 
-            def player1(message : discord.Message) -> bool:
-                accepted = ['rock', 'r', 'paper', 'p', 'scissors', 's']
-                return message.author.id == member.id and not message.guild and message.content.lower() in accepted
+                if author == 'rock':
+                    return 'Its a tie. ooo'
 
-            try:
-                rps1 = await self.client.wait_for('message', check = player1, timeout = 120)
-            except TimeoutError:
-                return await ctx.send('You took too long.. :/')
+                elif author == 'paper':
+                    return f'{ctx.author.mention} won!'
 
-            await member.send('Decision Made.')
-            
-            if rps1.content.lower() in ['rock', 'r']:
-                pick = rps1.content.lower()
-            elif rps1.content.lower() in ['paper', 'p']:
-                pick = rps1.content.lower()
-            elif rps1.content.lower() in ['scissors', 's']:
-                pick = rps1.content.lower() 
+                elif author == 'scissors':
+                    return f'{target.mention} won!'
 
-            await ctx.author.send('Pick `rock`, `paper` or `scissors`')
-
-            def player2(message: discord.Message) -> bool:
-                accepted = ['rock', 'r', 'paper', 'p', 'scissors', 's']
-                return message.author.id == ctx.author.id and not message.guild and message.content.lower() in accepted
-
-            try:
-                rps2 = await self.client.wait_for('message', check = player2, timeout = 120) 
-            except TimeoutError:
-                ctx.send('You took too long :/')  
-
-            await ctx.author.send('Decision Made.') 
-
-            if rps2.content.lower() in ['rock', 'r']:
-                choice = rps2.content.lower()
-
-            elif rps2.content.lower() in ['paper', 'p']:
-                choice = rps2.content.lower()
-
-            elif rps2.content.lower() in ['scissors', 's']:
-                choice = rps2.content.lower()
+            if targeted == 'paper':
                 
+                if author == 'rock':
+                    return f'{target.mention} won!'
 
+                elif author == 'paper':
+                    return 'its a tie. ooo'
 
-            embed = discord.Embed(
-                colour = 0xadd8e6,
-                description = f'{ctx.author.mention} picked **{choice.lower()}** and **{pick.lower()}** was picked by {member.mention}' 
+                elif author == 'scissors':
+                    return f'{ctx.author.mention} won!'
+
+            if targeted == 'scissors':
+                
+                if author == 'rock':
+                    return f'{ctx.author.mention} won!'
+
+                elif author == 'paper':
+                    return f'{target.mention} won!'
+
+                elif author == 'scissors':
+                    return 'Its a tie. ooo'
+
+        # <!-- -->
+
+        if target:
+
+            #send the start message
+            await ctx.send(embed = start)
+
+            #send the message to both players
+            await ctx.author.send("Opponent making a decision")
+            player1 = await pick(target)
+            player2 = await pick(ctx.author)
+
+            who_won = discord.Embed(
+                color = 0x00FFEC,
+                description = f'''{target.mention} picked **{player1}** and **{player2}** was picked by {ctx.author.mention}
+
+                **{win(player1, player2)}**'''
             )
 
-            await ctx.send(embed = embed)
+            await ctx.send(embed = who_won)
 
-            def win(choice, pick):
-                    
-                if pick == 'rock':
-                    if choice == 'rock':
-                        return "It's a draw!"
-                    elif choice == 'paper':
-                        return f'{ctx.author.mention} won!'
-                    elif choice == 'scissors':
-                        return f'{member.mention} won!'
+            
 
-                elif pick == 'paper':
-                    if choice == 'rock':
-                        return f"{member.mention} won!"
-                    elif choice == 'paper':
-                        return "it's a draw!"
-                    elif choice == 'scissors':
-                        return f'{ctx.author.mention} won!'
-
-                elif pick == 'scissors':
-                    if choice == 'rock':
-                        return f"{ctx.author.mention} won!"
-                    elif choice == 'paper':
-                        return f'{member.mention} won!'
-                    elif choice == 'scissors':
-                        return "It's a draw!"
-
-            winner = discord.Embed(
-                colour = 0xadd8e6,
-                description = win(choice, pick)
-            )
-
-            await ctx.send(embed = winner)
-
-        else:
-
-            accepted = ['rock', 'paper', 'scissors']
-
-            com_choice = random.choice(accepted)
-
-            await ctx.send('Pick `rock`, `paper` or `scissors`')
-
-            def player(message : discord.Message):
-                return message.author.id == ctx.author.id and message.channel.id == ctx.channel.id and message.guild == ctx.guild and message.content.lower() in accepted 
-
-            send = await self.client.wait_for('message', check = player)
-
-            await ctx.send(f'You chose **{send.content}**, I chose **{com_choice}**')
-            await ctx.send('Who won? idk')
 
 def setup(client):
-    client.add_cog(rps(client))
+    client.add_cog(Rps(client))
