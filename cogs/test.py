15cd6d228a1592421e44786933efa2faf193299a
BrimCap <vandithvponad123@gmail.com>
2020-07-10 14:27:40 +0530

    Changed Test.py

--- cogs/test.py
+++ cogs/test.py
@@ -7,8 +7,31 @@ class test(commands.Cog):
         self.client = client
     
     @commands.command()
-    async def test(self, ctx):
-        await ctx.send('I do work! Yay.')
+    async def test(self, ctx, member : discord.Member = None):
+
+        member = ctx.author if not member else member
+        
+        embed = discord.Embed(
+            color = 0x0000FF,
+            description = ''' Ok notice how i dont have a title.
+
+            I just did ```py
+            embed_name = discord.Embed(
+                color = 0x0000FF,
+                description = this.
+            )
+            ```
+
+            And what we will be making today is the thing in the top.
+            Look it says the name of the person i pinged but in a cool way.
+
+            We will be making that today. (It's really easy btw)
+            '''
+        )
+
+        embed.set_author(name = member.name, icon_url = member.avatar_url)
+
+        await ctx.send(embed = embed)
 
 def setup(client):
     client.add_cog(test(client))
