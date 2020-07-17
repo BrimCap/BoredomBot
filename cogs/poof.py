523de13088e703f6db9130b09659a935707dafec
BrimCap <vandithvponad123@gmail.com>
2020-07-17 10:59:38 +0530

    poof: Polished up code

--- cogs/poof.py
+++ cogs/poof.py
@@ -6,20 +6,35 @@ class Poof(commands.Cog):
     def __init__(self, client):
         self.client = client
 
-    @commands.command()
+    
+    @commands.command(aliases = ['kick'])
     async def poof(self, ctx, member : discord.Member, *, reason = None):
+
+        async def too_powerful():
+            await ctx.send(f"I'm sorry my master, but {member.mention} is too powerful.")
+            
 
         if member.id == self.client.user.id:
             await ctx.send('Did you just poof me? smh')
+
+        elif member.id == ctx.guild.owner_id:
+            await too_powerful()
+            return
+
+        elif ctx.me.top_role < member.top_role:
+            await too_powerful()
+            return
+            
         else:
 
             try:
+                await member.send(f'You have been poofed sadly. \nReason: ```{reason}```')
                 await member.kick(reason = reason)
-                await ctx.send(f':ok_hand: Just poofed {member.mention}')
-                await member.send('You have been poofed sadly')
 
-            except:
-                await ctx.send(f"I'm sorry my master, but {member.mention} is too powerful.")
+            except discord.Forbidden:
+                await too_powerful()
+            else:
+                await ctx.send(f':ok_hand: Just poofed {member.mention}')
 
     @poof.error
     async def poof_error_handler(self, ctx, error):
@@ -27,6 +42,9 @@ class Poof(commands.Cog):
         if isinstance(error, commands.MissingRequiredArgument):
             if error.param.name == 'member':
                 await ctx.send("U didn't ping anyone to poof. smh")
+
+        else:
+            raise error
 
 def setup(client):
     client.add_cog(Poof(client))
