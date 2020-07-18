f9f9c083f7f9c3e48bde1047c32cc7cf09aff375
BrimCap <vandithvponad123@gmail.com>
2020-07-18 18:52:48 +0530

    krunker: Made it so that you can close the while loop

--- cogs/krunker.py
+++ cogs/krunker.py
@@ -11,62 +11,75 @@ class Krunker(commands.Cog):
 
     def __init__(self, client):
         self.client = client
+        self.playing = False
 
-    @commands.command(aliases = ['krunk', 'invite', 'k', 'play'])
-    async def krunker(self, ctx, link):
+    @commands.Cog.listener()
+    async def on_message(self, message):
 
-        if not link.startswith('https://krunker.io'):
-            await ctx.send("This is not a valid link. Sad")
-            return
+        if message.content.startswith('https://krunker.io/'):
 
-        gamelink = link[25:]
+            self.playing = True
 
-        await ctx.message.delete()
+            link = message.content
 
-        res = requests.get("https://matchmaker.krunker.io/game-info?game=" + gamelink)
+            gamelink = link[25:]
 
-        info = json.loads(res.text)
-
-        game = discord.Embed(
-            colour = 0xfad15f,
-            title = 'Krunker Game Invite',
-            description = link
-        )
-
-        game.add_field(name = "Map", value = info[4]['i'])
-        game.add_field(name = "Players", value = f"{info[2]} / {info[3]}", inline = True)
-
-        send = await ctx.send(embed = game)
-
-        while True:
+            await message.delete()
 
             res = requests.get("https://matchmaker.krunker.io/game-info?game=" + gamelink)
 
             info = json.loads(res.text)
 
-            if res.text == '{"error":"InvalidGameId"}':
-                
-                end_message = discord.Embed(
-                    colour = 0xfad15f,
-                    description = 'The game end. Sad'
-                )
-
-                await send.edit(embed = end_message)
-                break
-                
-
-            update = discord.Embed(
-               colour = 0xfad15f,
-               title = 'Krunker Game Invite',
-               description = link
+            game = discord.Embed(
+                colour = 0xfad15f,
+                title = 'Krunker Game Invite',
+                description = link
             )
 
-            update.add_field(name = "Map", value = info[4]['i'])
-            update.add_field(name = "Players", value = f"{info[2]} / {info[3]}", inline = True)
+            game.add_field(name = "Map", value = info[4]['i'])
+            game.add_field(name = "Players", value = f"{info[2]} / {info[3]}", inline = True)
 
-            await send.edit(embed = update)
+            send = await message.channel.send(embed = game)
 
-            await asyncio.sleep(5)
+            while self.playing:
+
+                res = requests.get("https://matchmaker.krunker.io/game-info?game=" + gamelink)
+
+                info = json.loads(res.text)
+
+                if res.text == '{"error":"InvalidGameId"}':
+                
+                    break
+                
+
+                update = discord.Embed(
+                   colour = 0xfad15f,
+                   title = 'Krunker Game Invite',
+                   description = link
+                )
+
+                update.add_field(name = "Map", value = info[4]['i'])
+                update.add_field(name = "Players", value = f"{info[2]} / {info[3]}", inline = True)
+
+                await send.edit(embed = update)
+
+                await asyncio.sleep(5)
+
+            end_message = discord.Embed(
+                colour = 0xfad15f,
+                description = 'The game end or was closed. Sad'
+            )
+
+            await send.edit(embed = end_message)
+
+    @commands.command(aliases = ['stop'])
+    async def close(self, ctx):
+        if not self.playing:
+            await ctx.send('No active game found')
+            return
+
+        else:
+            self.playing = False
 
 def setup(client):
     client.add_cog(Krunker(client))
