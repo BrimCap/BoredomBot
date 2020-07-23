bc76a6b2d76ae84f9b31569caf270619c734aa77
BrimCap <vandithvponad123@gmail.com>
2020-07-23 17:13:11 +0530

    added image

--- cogs/krunker.py
+++ cogs/krunker.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 
 import requests
+import bs4
 
 import json
 
@@ -9,12 +10,36 @@ import asyncio
 
 class Krunker(commands.Cog):
 
+    rotation_maps = [
+        'Burg',
+        'Littletown',
+        'Sandstorm',
+        'Subzero',
+        'Undergrowth',
+        'Shipyard',
+        'Freight',
+        'Lostworld',
+        'Citadel',
+        'Oasis',
+        'Kanji',
+        'Newtown',
+        'Industry'
+    ]
+
     def __init__(self, client):
         self.client = client
         self.playing = False
 
     @commands.Cog.listener()
     async def on_message(self, message):
+
+        def remove_gamemode(game):
+
+            game_split = game.split('_')
+
+            gamemode = len(game_split[0]) + 1
+
+            return game[gamemode:]
 
         if message.content.startswith('https://krunker.io/?game='):
 
@@ -39,6 +64,17 @@ class Krunker(commands.Cog):
             game.add_field(name = "Map", value = info[4]['i'])
             game.add_field(name = "Players", value = f"{info[2]} / {info[3]}", inline = True)
 
+            if info[4]['m'] == 0:
+                map_name = remove_gamemode(info[4]['i'])
+
+                map_number = self.rotation_maps.index(map_name)
+
+                game.set_image(url = 'https://krunker.io/img/maps/map_' + str(map_number) + '.png')
+
+            else:
+                game.set_image(url = 'https://user-assets.krunker.io/m' + str(info[4]['m']) + '/thumb.png?v=5')
+            
+
             send = await message.channel.send(embed = game)
 
             while self.playing:
@@ -61,9 +97,19 @@ class Krunker(commands.Cog):
                 update.add_field(name = "Map", value = info[4]['i'])
                 update.add_field(name = "Players", value = f"{info[2]} / {info[3]}", inline = True)
 
+                if info[4]['m'] == 0:
+                    map_name = remove_gamemode(info[4]['i'])
+
+                    map_number = self.rotation_maps.index(map_name)
+
+                    update.set_image(url = 'https://krunker.io/img/maps/map_' + str(map_number) + '.png')
+
+                else:
+                    update.set_image(url = 'https://user-assets.krunker.io/m' + str(info[4]['m']) + '/thumb.png?v=5')
+
                 await send.edit(embed = update)
 
-                await asyncio.sleep(5)
+                await asyncio.sleep(20)
 
             end_message = discord.Embed(
                 colour = 0xfad15f,
